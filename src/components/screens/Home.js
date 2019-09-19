f5cbd708cc06bf84331473a8342125d0d808e79e
David <allhits@gmail.com>
2019-09-19 00:52:11 -0400

    Add restart button

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -7,7 +7,6 @@ import config from '../../config'
 //Should cache some gif assets and have an animation running in the background that looks like daytime and one for night when the game is over
 //Font for score
 //Balloon pop animation
-//Add reset button
 
 class Home extends React.Component {
 
@@ -24,6 +23,16 @@ class Home extends React.Component {
         this.decreaseLife = this.decreaseLife.bind(this)
         this.increaseLife = this.increaseLife.bind(this)
         this.blowUpSnowflake = this.blowUpSnowflake.bind(this)
+    }
+
+    restart() {
+        this.setState({
+            balloonNumber: 0,
+            tempBalloonNumber: 0,
+            score: 0,
+            life: 3,
+            revealSkull: false
+        })
     }
 
     decreaseScore() {
@@ -59,7 +68,8 @@ class Home extends React.Component {
 
     componentDidMount() {
         (control = () => {
-            let timer = Math.random() * (2000 - (this.state.balloonNumber))
+            let timer = Math.random() * (1200 - (this.state.balloonNumber))
+            console.log(timer)
             this.setState({
                 balloonNumber: this.state.life > 0 ? this.state.balloonNumber + 1 : this.state.balloonNumber,
                 tempBalloonNumber: this.state.life > 0 ? this.state.tempBalloonNumber + 1 : this.state.tempBalloonNumber
@@ -82,13 +92,22 @@ class Home extends React.Component {
 
         return(
             <View>
-                <TouchableOpacity style={{width: config.screenWidth, height: config.screenHeight}} onPressIn={() => this.decreaseScore()} activeOpacity={this.state.life > 0 ? 0 : 1}>
-                <View style={{marginTop: 40, opacity: .04}}><MaterialCommunityIcons name={'skull'} size={config.screenWidth} color={'rgb(0,0,0)'}/></View>
-                <View style={[styles.container, {opacity: this.state.revealSkull ? 0 : 1, backgroundColor: this.state.life > 0 ? 'rgb(239,239,255)' : 'rgb(0,64,128)'}]}>{allBalloons}</View>
-                <View style={styles.scoreBar}>
-                    <Text style={styles.score}>{this.state.life > 0 ? 'Score: ' : 'Final Score: '}{this.state.score}</Text>
-                    <Text style={styles.lives}>{lives}</Text>
-                </View>
+                <TouchableOpacity style={{width: config.screenWidth, height: config.screenHeight, backgroundColor: this.state.life > 0 ? null : 'rgb(0,64,128)'}} onPressIn={() => this.decreaseScore()} activeOpacity={this.state.life > 0 ? 0 : 1}>
+                { this.state.life > 0 ? null :
+                    <View style={{alignItems: 'center', marginTop: config.screenWidth/3}}>
+                        <Text>Final Score: {this.state.score}</Text>
+                        <TouchableOpacity onPress={() => this.restart()} style={{width: config.screenWidth/4}}><MaterialCommunityIcons name={'backup-restore'} size={config.screenWidth/4} color={'rgb(0,0,0)'}/></TouchableOpacity>
+                    </View> }
+                {this.state.life > 0 ?
+                    <View style={{marginTop: 40, opacity: .04, backgroundColor: 'rgb(255,255,255)'}}>
+                        <MaterialCommunityIcons name={'skull'} size={config.screenWidth} color={'rgb(0,0,0)'}/>
+                    </View> : null}
+                {this.state.life > 0 ? <View style={[styles.container, {opacity: this.state.revealSkull ? 0 : 1, backgroundColor: 'rgb(239,239,255)'}]}>{allBalloons}</View> : null}
+                {this.state.life > 0 ?
+                    <View style={styles.scoreBar}>
+                        <Text style={styles.score}>Score: {this.state.score}</Text>
+                        <Text style={styles.lives}>{lives}</Text>
+                    </View> : null}
                 </TouchableOpacity>
             </View>
         )
