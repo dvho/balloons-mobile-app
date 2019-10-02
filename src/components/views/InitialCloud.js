1d8d21fc9f545a388cb2c94c54c5ce86f44d63d0
David <allhits@gmail.com>
2019-10-02 12:21:15 -0400

    Remove finished clouds from virtual DOM, otherwise face performance issues

--- src/components/views/InitialCloud.js
+++ src/components/views/InitialCloud.js
@@ -8,7 +8,7 @@ import config from '../../config'
 //AndDesign - cloud
 //Entypo - cloud
 //FontAwesome - cloud
-//Foundation - cloud
+//Foundation - cloud (this cloud isn't nice - I removed it)
 //Ionicons - ios-cloud, ios-cloudy, md-cloud
 //MaterialIcons - cloud
 //MaterialIcons - wb-cloudy
@@ -19,6 +19,7 @@ class InitialCloud extends React.PureComponent {
     constructor() {
         super()
         this.state = {
+            finishedCloud: false //Need to trigger this qualifier in state and use it to remove clouds from the virtual DOM with a display 'none' in an inline ternary operator, otherwise the off-screen clouds keep re-rendering and the game fatally stutters and lags.
         }
         this.clouds = [{suite: AntDesign, name: 'cloud'}, {suite: Entypo, name: 'cloud'}, {suite: FontAwesome, name: 'cloud'}, {suite: Ionicons, name: 'ios-cloud'}, {suite: Ionicons, name: 'ios-cloudy'}, {suite: Ionicons, name: 'md-cloud'}, {suite: MaterialIcons, name: 'cloud'}, {suite: MaterialIcons, name: 'wb-cloudy'}, {suite: MaterialCommunityIcons, name: 'apple-icloud'}, {suite: MaterialCommunityIcons, name: 'cloud'}]
     }
@@ -38,14 +39,14 @@ class InitialCloud extends React.PureComponent {
         Animated.timing(
           left,
           { toValue: 0 - config.screenWidth/size,
-            duration: 50000 + Math.random() * 50000,
+            duration: 60000 + Math.random() * 60000,
             easing: Easing.bezier(0, 0, 1, 1), //this is linear because the default apparently isn't
             userNativeDriver: true //this needs to be added for Android
-        }).start()
+        }).start(() => this.setState({finishedCloud: true}))
 
         return(
-            <Animated.View style={{left: left, position: 'absolute', width: 100 + '%', height: 100 + '%', justifyContent: 'flex-end'}}>
-                <IconComponent name={name} size={config.screenWidth/size} style={{bottom: config.screenWidth * .25, color: `hsl(${color}, 100%, 97%))`, opacity: opacity, shadowColor: 'rgb(255,255,255)', shadowOpacity: 1, shadowOffset: {width: 0, height: 0}, shadowRadius: 10}}/>
+            <Animated.View style={{display: this.state.finishedCloud ? 'none' : null, left: left, position: 'absolute', width: 100 + '%', height: 100 + '%', justifyContent: 'flex-end'}}>
+                <IconComponent name={name} size={config.screenWidth/size} style={{bottom: config.screenWidth * .25, color: `hsl(${color}, 100%, 94%))`, opacity: opacity, shadowColor: 'rgb(255,255,255)', shadowOpacity: 1, shadowOffset: {width: 0, height: 0}, shadowRadius: 10}}/>
             </Animated.View>
         )
     }
